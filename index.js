2aaf8c11855e66e63d786c6319b76bcb1857fdd8
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-26 21:37:26 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return [
+       drivers[0],
+       drivers[1]
+    ]
+}
+
+function returnLastTwoDrivers(drivers){
+    return [
+        drivers[drivers.length-2],
+        drivers[drivers.length-1]
+    ]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, callback) => {
+    return callback(drivers)
+}
